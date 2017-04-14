715394ad21d085c2f0b58442961150dd8971ffea
dkorolev <ue73062@sbb.ch>
2017-04-14 19:10:17 +0300

    new objects for Reader

---
+++ src/main/java/com/example/domain/Reader.java
@@ -0,0 +1,81 @@
+package com.example.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+/**
+ * @author dkorolev
+ *         Date: 11.04.2017
+ *         Time: 19:27
+ */
+@Entity
+public class Reader implements UserDetails{
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private String username;
+    private String fullname;
+    private String password;
+
+
+    /** Getters & setters **/
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+
+    //User details methods
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority("READER"));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
