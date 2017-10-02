23189758c7a4fe5d6d9653932df5e3807c80ace7
dkorolev <korwork87@yahoo.com>
2017-10-02 17:46:01 +0300

    external attribute

--- src/main/java/com/example/controllers/ReadingListController.java
+++ src/main/java/com/example/controllers/ReadingListController.java
@@ -1,23 +1,26 @@
 package com.example.controllers;
 
 
-import java.util.List;
-
 import com.example.domain.Book;
 import com.example.jpa.ReadingListRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/readingList")
+@ConfigurationProperties(prefix="amazon")
 public class ReadingListController {
 
 
     private static String USER = "Tom";
+
+    private String associatedId;
 
 
     private ReadingListRepository readingListRepository;
@@ -29,6 +32,7 @@ public class ReadingListController {
         List<Book> readingList = readingListRepository.findByReader(USER);
         if (readingList != null) {
             model.addAttribute("books", readingList);
+            model.addAttribute("amazonID", associatedId);
         }
         return "readingList";
     }
@@ -45,5 +49,9 @@ public class ReadingListController {
     @Autowired
     public void setReadingListRepository(ReadingListRepository readingListRepository) {
         this.readingListRepository = readingListRepository;
+    }
+
+    public void setAssociatedId(String associatedId) {
+        this.associatedId = associatedId;
     }
 }
