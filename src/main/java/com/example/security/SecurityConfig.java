965c5b82d8a1c0fad9d6f222dfe51b1a99cb738c
dkorolev <korwork87@yahoo.com>
2017-10-02 19:42:53 +0300

    spring profiles + remove logback.xml

--- src/main/java/com/example/security/SecurityConfig.java
+++ src/main/java/com/example/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.example.domain.Reader;
 import com.example.jpa.ReaderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -18,6 +19,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
  *         Date: 11.04.2017
  *         Time: 19:24
  */
+@Profile("production")
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
